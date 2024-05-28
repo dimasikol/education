c533845d84a25891df618f19305f632def48d021
dimasikol <65474400+dimasikol@users.noreply.github.com>
2024-05-28 09:56:31 +0300

    Update structure.py

--- structure.py
+++ structure.py
@@ -1,1 +1,20 @@
+class NodeLinkList:
+    def __init__(self, value:int | float | str, next = None):
+        self.value = value
+        self.next = next
+        self.previous = None
+    def __repr__(self):
+        return f"{self.value}->{self.next}"
+
+
+class LinkList:
+  def __init__(self, value = None):
+  
+
+
+  def check_type(self,value):
+    if isinstance(value, (int,float,str):
+        pass
+    elif isinstance(value,(iterable)):
+
 

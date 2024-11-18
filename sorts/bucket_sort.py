0e5297ebefb2688e1bf7b4f3364b2cbcc819079a
dimasikol <65474400+dimasikol@users.noreply.github.com>
2024-11-18 19:36:55 +0300

    Update bucket_sort.py

--- sorts/bucket_sort.py
+++ sorts/bucket_sort.py
@@ -1,20 +1,24 @@
-def bucket_sort(arr: list[int | float | str]) -> list[int | float | str]:
-    if len(arr)==2:
-        return [min(arr),max(arr)]
-    if len(arr)<=1:
-        return arr
-    mx = mn = arr[0]
-    for i in range(len(arr)):
-        if mx < arr[i]:
-            mx = arr[i]
-        if mn > arr[i]:
-            mn = arr[i]
-    seed = (mx+mn)//2
-    min_bucklet = []
-    max_bucket = []
-    for i in arr:
-        if i>seed:
-            max_bucket.append(i)
+def bucket_sort(array):
+    if len(array) == 2:
+        return [min(array), max(array)]
+    elif len(array) <= 1:
+        return array
+    mx = mn = array[0]
+    for i in range(len(array)):
+        if array[i] > mx:
+            mx = array[i]
+        if array[i] < mn:
+            mn = array[i]
+    seed = (mx + mn) // 2
+    bucket_min = []
+    bucket_mid = []
+    bucket_max = []
+    for i in range(len(array)):
+        if array[i] > seed:
+            bucket_max.append(array[i])
+        elif array[i]==seed:
+            bucket_mid.append(array[i])
         else:
-            min_bucklet.append(i)
-    return bucket_sort(min_bucklet)+bucket_sort(max_bucket)
+            bucket_min.append(array[i])
+    return bucket_sort(bucket_min) +bucket_mid+ bucket_sort(bucket_max)
+

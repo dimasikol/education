ce2e5051111f63125036bec6541ab317a8ef0089
dimasikol <65474400+dimasikol@users.noreply.github.com>
2025-01-26 17:19:14 +0300

    Update sorts.py

--- sorts.py
+++ sorts.py
@@ -164,7 +164,32 @@ class Sorts:
             step *= 10
             mx //= 10
         return array
-
+   
+    @staticmethod
+    def cube_sort(arr: list[int]):
+        data = {}
+        result = []
+        for num in arr:
+            pref = 1
+            if num < 0:
+                pref = -1
+            sqr2 = pref * math.isqrt(num*pref)
+            sqr3 = pref * math.isqrt(math.isqrt(num*pref))
+            if sqr3 in data:
+                if sqr2 in data[sqr3]:
+                    data[sqr3][sqr2].append(num)
+                else:
+                    data[sqr3][sqr2] = [num,]
+            else:
+                data[sqr3] = {}
+                data[sqr3].update({sqr2:[num]})
+        data_3 = [i for i in data]
+        for sq3 in counter_sort(data_3):
+            data_2 = [i for i in data[sq3]]
+            for sq2 in counter_sort(data_2):
+                result += counter_sort(data[sq3][sq2])
+        return result
+    
     @staticmethod
     def merge_sort(array: list[int | str | float], clean_func=False) -> list[int | str | float]:
         if clean_func:
@@ -231,13 +256,6 @@ class Sorts:
                 bucket_min.append(array[i])
         return bucket_sort(bucket_min) +bucket_mid+ bucket_sort(bucket_max)
     
-    @staticmethod
-    def cube_sort(array):
-        pass
-
-    @staticmethod
-    def counter_sort(array):
-        pass
     
     class Timer(object):
         def __init__(self, target):

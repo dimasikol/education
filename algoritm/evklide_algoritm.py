d0bd6808d2da2f47e6a94f4ce15e840208038f62
dimasikol <65474400+dimasikol@users.noreply.github.com>
2025-03-31 11:28:04 +0300

    Create evklide_algoritm.py

---
+++ algoritm/evklide_algoritm.py
@@ -0,0 +1,9 @@
+"""NOD"""
+def NOD(num1,num2):
+  if num1 == 0 or num2 == 0:
+    return num1+num2
+  if num1>num2:
+    return NOD(num1%num2,num2)
+  return NOD(num1,num2%num1)
+def NOK(num1,num2):
+  return num1*num2/NOD(num1,num2)
